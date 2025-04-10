8baccfcea1a103ecffed5ce7885c8a3fcd73b385
Gustavo217BGP <guscruz217@gmail.com>
2025-04-10 15:18:00 +0000

    desafiomestre

--- rfgf.c
+++ rfgf.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
-
+#include <time.h>
+#include <stdlib.h>
 int main() {
-char nome[50], codigo[50], estado[50];
+
+char nome[50], codigo[50], estado[50], tipocomparacao, tipodeataque;
 int  populacao, pontosturisticos;
-float areakm, densidadepop, pibpercapta, pib;
+float areakm, densidadepop, pibpercapta, pib, resultado;
+printf("escolha entre ataque(A), defesa(D) ou recuo(R):");
+scanf(" %c", &tipocomparacao);
+
+printf("escolha entre densidade populacional(D), pib per capta(P) ou numero de pontos turisticos(T) para atacar: ");
+scanf(" %c", &tipodeataque);
 
 printf ("Digite o nome do estado:  ");
 scanf ("%s", estado);
@@ -28,23 +35,6 @@ scanf ("%f", &areakm);
 
 densidadepop = populacao / areakm;
 pibpercapta = pib / populacao;
-
-printf("Cidade: %s \n", nome);
-printf("Estado: %s \n", estado);
-printf("Pontos turisticos: %d \n", pontosturisticos); 
-printf("População: %d \n", populacao);
-
-printf("Área km²: %f\n", areakm);
-
-printf("PIB: %f \n", pib);
-
-printf("Código da carta: %s\n", codigo);
-
-printf("A densidade populacional é: %f\n", densidadepop);
-
-printf("O pib per capta é: %f\n\n", pibpercapta);
-
-
 
 
 char nome2[50], codigo2[50], estado2[50];
@@ -72,18 +62,54 @@ scanf ("%f", &pib2);
 printf("Digite a area km²: ");
 scanf ("%f", &areakm2);
 
-densidadepop2 = populacao / areakm;
-pibpercapta2 = pib2 / populacao;
+densidadepop2 = populacao2 / areakm2;
+pibpercapta2 = pib2 / populacao2;
 
-printf("Cidade: %s\n", nome2);
-printf("Estado: %s\n", estado2);
-printf("Pontos turisticos: %d\n", pontosturisticos2); 
-printf("População: %f\n", populacao2);
-printf("Área km²: %f\n", areakm2);
-printf("PIB: %f\n", pib2);
-printf("Código da carta: %s\n", codigo2);
-printf("A densidade populacional é: %f\n", densidadepop2);
-printf("O pib per capta é: %f\n\n", pibpercapta2);
+
+
+switch (tipocomparacao)
+{
+ case 'A':
+ case 'a':
+printf("Você escolheu a opção ataque!\n");
+break;
+case 'D':
+case 'd':
+printf("voce escolheu a opção defesa!\n");
+break;
+case 'R':
+case 'r':
+printf("você escolheu a opção recuo!\n");
+break;
+}
+
+switch(tipodeataque)
+{
+case'D':
+case'd':
+printf("você quer atadar usando a densidade populacional!\n");
+printf("a sua densidade populacional é:%f e a do adversario é %f\n", densidadepop,densidadepop2);
+resultado = densidadepop < densidadepop2 ? 1:0;
+break;
+case'P':
+case'p':
+printf("voce quer atacar usando o pib percapta!\n");
+printf("a sua pib per capta é %f e a do adversario é %f\n", pibpercapta, pibpercapta2);
+resultado = pibpercapta > pibpercapta2 ? 1:0;
+break;
+case 'T':
+case 't':
+printf("você quer atacar usando os pontos turisticos!\n");
+printf("o seu numero de pontos turisticos é %d e odo adversario é %d\n", pontosturisticos, pontosturisticos2);
+resultado = pontosturisticos > pontosturisticos2 ? 1:0;
+break;
+}
+if(resultado == 1){
+printf("você venceu!!!\n");
+}else{
+printf("você perdeu!\n");
+}
+
 
 
 
